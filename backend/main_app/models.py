e9c1887c6db45c426103627dcfa9e4d1293338c1
Moaaz Hamdy <moaaz.m.hamdy@gmail.com>
2023-02-18 18:53:12 +0200

    Added models

--- backend/main_app/models.py
+++ backend/main_app/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Examiner(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    examiner = models.ForeignKey(Examiner, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+    
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    enrollment_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    def __str__(self):
+        return self.student.name + " " + self.course.name
+
+class Exam(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    exam_start_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    exam_end_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    duration = models.DurationField()
+    max_grade = models.IntegerField()
+    def __str__(self):
+        return self.name
+    
+class Question(models.Model):
+    question_text = models.TextField()
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    correct_answer = models.CharField(max_length=1)
+    def __str__(self):
+        return self.question
+    
+class Choice(models.Model):
+    choice_text = models.TextField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_number = models.CharField(max_length=1)
+    def __str__(self):
+        return self.choice
+    
+class Attempt(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    start_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    submission_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    grade = models.IntegerField()
+    def __str__(self):
+        return self.student.name + " " + self.exam.name
