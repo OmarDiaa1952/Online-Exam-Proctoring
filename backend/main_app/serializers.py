afc8faac41082d7d886863385d20eb7f2289594e
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-02-19 12:13:57 +0200

    adding modelSerializers

---
+++ backend/main_app/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from .models import Exam, Student, Examiner, Course, Enrollment, Question, Attempt, Choice
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = "__all__"
+
+class ExaminerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Examiner
+        fields = "__all__"
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = "__all__"
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enrollment
+        fields = "__all__"
+
+class ExamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exam
+        fields = "__all__"
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = "__all__"
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = "__all__"
+
+class AttemptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attempt
+        fields = "__all__"
