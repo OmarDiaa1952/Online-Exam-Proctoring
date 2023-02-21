8d70d2921b7efc38a1929819854ece4dc039dac6
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-02-21 17:40:17 +0200

    editing course views

--- backend/main_app/urls.py
+++ backend/main_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", CourseView.as_view(), name="course"),
+    path("courselist", CourseListView.as_view(), name="course"),
 ]
--- backend/main_app/views.py
+++ backend/main_app/views.py
@@ -6,12 +6,22 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-# Trying to create a new Course
-class CourseView(generics.ListCreateAPIView):
+class CourseListView(generics.ListCreateAPIView): 
+    """
+    Lists all courses in DB
+    i don't know if this should return all courses or just courses related to specific examiner or student
+    or should we have many functions for those different cases?
+    """
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-class CreateCourseView(APIView):
+class CourseDetailView(generics.RetrieveUpdateDestroyAPIView):
+    # here i want to return details of a specific requested course
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class CreateCourseView(APIView): 
+    # this view is responsible for creating a new course by the examiner
     queryset = Course.objects.all()
     serializer_class = CreateCourseSerializer
 
@@ -30,7 +40,7 @@ class CreateCourseView(APIView):
             'message': 'Course could not be created with received data.'
         }, status=status.HTTP_400_BAD_REQUEST) 
     
-class ExamView(generics.ListCreateAPIView):
+class ExamListView(generics.ListCreateAPIView):
     queryset = Exam.objects.all()
     serializer_class = ExamSerializer
 
