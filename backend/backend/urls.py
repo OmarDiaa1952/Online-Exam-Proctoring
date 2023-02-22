ea10c03065214e3922895b4d50b169bc48a74d4c
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-02-22 22:14:56 +0200

    adding jwt setup

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('', include("main_app.urls")),
 ] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
