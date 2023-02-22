5f7fcd7e87fc453a48d573b53f4c83655a9b5ac4
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-02-22 12:03:18 +0200

    adding respone examples file

---
+++ response.py
@@ -0,0 +1,22 @@
+{
+    "responses": [ 
+        {# in case object is created correctly on registration
+            # http 201 created
+            "name": "mwatheq",
+            "email": "watheq@gmail.com",
+            "password": "123"
+        },
+        {# in case object is not created correctly on registration
+            # http 400 bad request
+            "name": [
+                "This field may not be blank."
+            ],
+            "email": [
+                "This field may not be blank."
+            ],
+            "password": [
+                "This field may not be blank."
+            ]
+        },
+    ]
+}
