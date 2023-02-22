2204dc0ca5fafeaa2d4c99df18ccccabf1933c55
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-02-22 15:25:40 +0200

    simple edit

--- backend/main_app/urls.py
+++ backend/main_app/urls.py
@@ -4,7 +4,6 @@ from .views import *
 urlpatterns = [
     path("examinerregister", ExaminerRegisterView.as_view(), name="ExaminerRegister"),
     path("studentregister", StudentRegisterView.as_view(), name="StudentRegister"),
-    path("courselist", CourseListView.as_view(), name="course"),
+    path("courselist", StudentCourseListView.as_view(), name="course"),
     path("coursejoin", JoinCourseView.as_view(), name="coursejoin"),
-
 ]
