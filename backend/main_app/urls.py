d98df6b968fe7b57b3bf60bd471906f83f657bac
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-02-22 11:45:19 +0200

    adding registration urls

--- backend/main_app/urls.py
+++ backend/main_app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path("examinerregister", ExaminerRegisterView.as_view(), name="ExaminerRegister"),
+    path("studentregister", StudentRegisterView.as_view(), name="StudentRegister"),
     path("courselist", CourseListView.as_view(), name="course"),
     path("coursejoin", JoinCourseView.as_view(), name="coursejoin"),
+
 ]
