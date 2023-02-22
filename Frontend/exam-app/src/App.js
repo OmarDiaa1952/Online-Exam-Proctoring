5407710d63c63a288ea3c8e5ca240396fa36ecd8
Ahmed Alaa <1808283@eng.asu.edu.eg>
2023-02-22 11:28:26 +0200

    Added Routes for all pages

--- Frontend/exam-app/src/App.js
+++ Frontend/exam-app/src/App.js
@@ -2,6 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import WelcomePage from "./pages/Welcome";
 import RegistrationPage from "./pages/Registration";
 import HomePage from "./pages/Home";
+import ModifyCoursePage from "./pages/ModifyCourse"
+import CoursePage from "./pages/Course";
+import CourseDetailsPage from "./pages/CourseDetails";
+import ExamPage from "./pages/Exam";
+import ExamDetailsPage from "./pages/ExamDetails";
+import EditExamPage from "./pages/EditExam";
+import LogFilePage from "./pages/LogFile";
 
 function App() {
   return (
@@ -9,6 +16,13 @@ function App() {
       <Route path="/" exact element={<WelcomePage />} />
       <Route path="/register" exact element={<RegistrationPage />} />
       <Route path="/home" exact element={<HomePage />} />
+      <Route path="/modify-course" exact element={<ModifyCoursePage />} />
+      <Route path="/course" exact element={<CoursePage />} />
+      <Route path="/course-details" exact element={<CourseDetailsPage />} />
+      <Route path="/exam" exact element={<ExamPage />} />
+      <Route path="/exam-details" exact element={<ExamDetailsPage />} />
+      <Route path="/edit-exam" exact element={<EditExamPage />} />
+      <Route path="/log-file" exact element={<LogFilePage />} />
     </Routes>
   );
 }
