7bc63c2281e6e62fa324902a96721623ce66f436
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-02-23 14:04:01 +0200

    adding token view

--- backend/main_app/views.py
+++ backend/main_app/views.py
@@ -5,7 +5,10 @@ from .models import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth import authenticate, login
+from rest_framework_simplejwt.views import TokenObtainPairView
 
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 class StudentRegisterView(generics.CreateAPIView):
     # this view is responsible for registering a student
@@ -17,7 +20,8 @@ class StudentRegisterView(generics.CreateAPIView):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(status=status.HTTP_201_CREATED)
+            #return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
