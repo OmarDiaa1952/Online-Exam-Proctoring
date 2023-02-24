1cdb0b6a93647b20dba532ffeb29375bcba2ce8b
Moaaz Hamdy <52506331+Mo3az2000@users.noreply.github.com>
2023-02-24 15:57:36 +0200

    Added admin view for user Models

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role')
+    list_filter = ('role',)
+    readonly_fields = ('date_joined', 'last_login', 'user_permissions', 'groups', 'is_staff', 'is_superuser')
+
+
+admin.site.register(Student)
+admin.site.register(Examiner)
+admin.site.register(User, UserAdmin)
+admin.site.register(StudentProfile)
+admin.site.register(ExaminerProfile)
