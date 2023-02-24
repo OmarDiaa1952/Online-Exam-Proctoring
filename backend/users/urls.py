c57d6daa9d8f7a2a156f79e23dc3db6d8d84a00d
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-02-24 20:42:21 +0200

    added all possble paths till now

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import *
 from rest_framework_simplejwt.views import TokenRefreshView
 
-
-
 urlpatterns = [
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("examinerregister", ExaminerRegisterView.as_view(), name="ExaminerRegister"),
     path("studentregister", StudentRegisterView.as_view(), name="StudentRegister"),
+    path("examinerlogin", ExaminerLoginView.as_view(), name="ExaminerLogin"),
+    path("studentlogin", StudentLoginView.as_view(), name="StudentLogin"),
 ]
