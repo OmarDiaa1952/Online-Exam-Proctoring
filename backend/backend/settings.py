4fdc69b665ddcfb3ab81a20cb9b8a36f676d599f
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-02-25 14:02:31 +0200

    token simple edit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -150,7 +150,7 @@ MEDIA_URL = '/media/'
 AUTH_USER_MODEL = 'users.User'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=500),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
