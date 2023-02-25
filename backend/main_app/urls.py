4a092defd13e965342d224a2dac28380ba9f025a
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-02-25 07:03:29 +0200

    adding all posible urls till now

--- backend/main_app/urls.py
+++ backend/main_app/urls.py
@@ -5,5 +5,16 @@ from .views import *
 
 urlpatterns = [
     path("courselist", StudentCourseListView.as_view(), name="course"),
-    path("coursejoin", JoinCourseView.as_view(), name="coursejoin"),
+    path("coursedetail", CourseDetailView.as_view(), name="coursedetail"),
+    # path("coursesearch", CourseSearchView.as_view(), name="coursesearch"),
+    # path("courseenrollment", CourseEnrollmentView.as_view(), name="courseenrollment"),
+    path("coursejoin", CourseJoinView.as_view(), name="coursejoin"),
+    # path("examdetail", ExamDetailView.as_view(), name="examdetail"),
+    # path("examstart", ExamStartView.as_view(), name="examstart"),
+    # path("examreview", ExamReviewView.as_view(), name="examreview"),
+    # path("examend", ExamEndView.as_view(), name="examend"),
+    # path("courseedit", CourseEditView.as_view(), name="editcourse"),
+    path("examcreate", ExamCreateView.as_view(), name="createexam"),
+    # path("examedit", ExamEditView.as_view(), name="editexam"),
+    # path("examdelete", ExamDeleteView.as_view(), name="deleteexam"),
 ]
