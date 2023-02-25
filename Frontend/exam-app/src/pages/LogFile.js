2c6a457ecf68f3b856c82a968f308c21e6967d2b
Ahmed Alaa <1808283@eng.asu.edu.eg>
2023-02-25 22:50:26 +0200

    Updated log file
    Created simple code for this page until it is updated later on

--- Frontend/exam-app/src/pages/LogFile.js
+++ Frontend/exam-app/src/pages/LogFile.js
@@ -1,7 +1,126 @@
+import { Link } from "react-router-dom";
+
 function LogFilePage() {
     return (
-        <div>Log File Page</div>
+        <div>
+            <h1>Log File</h1>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Student Name</th>
+                            <th>Email</th>
+                            <th>Start Date</th>
+                            <th>End Date</th>
+                            <th>Grade</th>
+                            <th>Cheating Instances</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>John Doe</td>
+                            <td>
+                                <a href="mailto:    ">    </a>
+                            </td>
+                            <td>2021-06-01 20:00:00</td>
+                            <td>2021-06-01 21:10:00</td>
+                            <td>100</td>
+                            <td>2021-06-01 20:00:00</td>
+                        </tr>
+                        <tr>
+                            <td>John Doe</td>
+                            <td>
+                                <a href="mailto:    ">    </a>
+                            </td>
+                            <td>2021-06-01 20:00:00</td>
+                            <td>2021-06-01 21:10:00</td>
+                            <td>100</td>
+                            <td>2021-06-01 20:00:00</td>
+                        </tr>
+                        <tr>
+                            <td>John Doe</td>
+                            <td>
+                                <a href="mailto:    ">    </a>
+                            </td>
+                            <td>2021-06-01 20:00:00</td>
+                            <td>2021-06-01 21:10:00</td>
+                            <td>100</td>
+                            <td>2021-06-01 20:00:00</td>
+                        </tr>
+                        <tr>
+                            <td>John Doe</td>
+                            <td>
+                                <a href="mailto:    ">    </a>
+                            </td>
+                            <td>2021-06-01 20:00:00</td>
+                            <td>2021-06-01 21:10:00</td>
+                            <td>100</td>
+                            <td>2021-06-01 20:00:00</td>
+                        </tr>
+                        <tr>
+                            <td>John Doe</td>
+                            <td>
+                                <a href="mailto:    ">    </a>
+                            </td>
+                            <td>2021-06-01 20:00:00</td>
+                            <td>2021-06-01 21:10:00</td>
+                            <td>100</td>
+                            <td>2021-06-01 20:00:00</td>
+                        </tr>
+                        <tr>
+                            <td>John Doe</td>
+                            <td>
+                                <a href="mailto:    ">    </a>
+                            </td>
+                            <td>2021-06-01 20:00:00</td>
+                            <td>2021-06-01 21:10:00</td>
+                            <td>100</td>
+                            <td>2021-06-01 20:00:00</td>
+                        </tr>
+                        <tr>
+                            <td>John Doe</td>
+                            <td>
+                                <a href="mailto:    ">    </a>
+                            </td>
+                            <td>2021-06-01 20:00:00</td>
+                            <td>2021-06-01 21:10:00</td>
+                            <td>100</td>
+                            <td>2021-06-01 20:00:00</td>
+                        </tr>
+                        <tr>
+                            <td>John Doe</td>
+                            <td>
+                                <a href="mailto:    ">    </a>
+                            </td>
+                            <td>2021-06-01 20:00:00</td>
+                            <td>2021-06-01 21:10:00</td>
+                            <td>100</td>
+                            <td>2021-06-01 20:00:00</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div>
+        <div>
+          <Link to="/preview-exam">
+            <button type="button">Back</button>
+          </Link>
+        </div>
+        <div>
+          <Link to={'/home'}>
+            <button type="button">Home</button>
+          </Link>
+        </div>
+      </div>
+        </div>
     );
 }
 
 export default LogFilePage;
+
+// This page contains a list of all the students in this course with the 
+// following data for each of them: student name, email, the date at which the student 
+// started the exam and ended, the grade, and the dates of the cheating instances suspects. 
+// The page also has a back button and a home page button.
+// • If the back button is pressed, the examiner is directed back to the Preview Exam page.
+// • If the home page button is pressed, the examiner returns to the home page
