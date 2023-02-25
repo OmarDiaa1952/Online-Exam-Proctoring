8914ac099ce8c8f90fbadeb7efa7dd017fc2a344
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-02-25 11:18:51 +0200

    cleaning users views

--- backend/users/views.py
+++ backend/users/views.py
@@ -24,28 +24,7 @@ class StudentRegisterView(generics.CreateAPIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# class StudentLoginView(APIView):
-#     # this view is responsible for logging in a student
-#     def post(self, request, format=None):
-#         email = request.data.get("email")
-#         password = request.data.get("password")
-#         user = Student.objects.filter(email=email).first()
-#         ###
-#         # Not working for some reason
-#         # print("password check")
-#         # print(user.check_password(password))
-#         # user = authenticate(email=email, password=password)
-#         # print("authenticate result")
-#         # print(user)
-#         ###
-#         if user.check_password(password):
-#             # login(request, user) # i think it works with sessions not tokens
-#             return Response({'message':'Login Successful'}, status=status.HTTP_200_OK)
-
-#         return Response({'message':'Login Failed'}, status=status.HTTP_400_BAD_REQUEST)
-    
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
 
 ########################### Examiner Views ###########################
 
@@ -61,18 +40,4 @@ class ExaminerRegisterView(generics.CreateAPIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# class ExaminerLoginView(APIView):
-#     # this view is responsible for logging in an examiner
-
-#     def post(self, request, format=None):
-#         email = request.data.get("email")
-#         password = request.data.get("password")
-#         user = Student.objects.filter(email=email).first()
-#         # AUTHENTICATE IS NOT WORKING PROPERLY 
-#         #  user = authenticate(email=email, password=password)
-#         if user.check_password(password):
-#             #login(request, user) # i think it works with sessions not tokens
-#             return Response({'message':'Login Successful'}, status=status.HTTP_200_OK)
-
-#         return Response({'message':'Login Failed'}, status=status.HTTP_400_BAD_REQUEST)
+        
