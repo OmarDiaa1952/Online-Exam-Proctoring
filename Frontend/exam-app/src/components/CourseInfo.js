e6ef64e64c8c2ac39d7115e36a4f15d5c76e5d4a
Ahmed Alaa <1808283@eng.asu.edu.eg>
2023-03-03 09:30:22 +0200

    Linked CourseDetails with backend
    Updated CourseDetailsPage and CourseInfo

--- Frontend/exam-app/src/components/CourseInfo.js
+++ Frontend/exam-app/src/components/CourseInfo.js
@@ -5,19 +5,19 @@ function CourseInfo(props) {
     <div>
       <div>
         <span>Course Name: </span>
-        <span>{props.courseData.courseName}</span>
+        <span>{props.courseData.name}</span>
       </div>
       <div>
         <span>Course ID: </span>
-        <span>{props.courseData.courseId}</span>
+        <span>{props.courseData.id}</span>
       </div>
       <div>
         <span>Examiner: </span>
-        <span>{props.courseData.examinerName}</span>
+        <span>{props.courseData.examiner_name}</span>
       </div>
       <div>
         <span>Description: </span>
-        <span>{props.courseData.courseDescription}</span>
+        <span>{props.courseData.description}</span>
       </div>
     </div>
   );
--- Frontend/exam-app/src/pages/CourseDetails.js
+++ Frontend/exam-app/src/pages/CourseDetails.js
@@ -1,54 +1,78 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 
 import CourseInfo from "../components/CourseInfo";
 import UserContext from "../store/user-context";
 
 function CourseDetailsPage() {
   let courseRequested = false;
-  let requestType = "requestCourse";
-  if(courseRequested) {
-    requestType = "cancelRequestCourse";
-  }
-  else requestType = "requestCourse";
   const userCtx = useContext(UserContext);
-  const email = userCtx.email;
-  const DUMMY_DATA = {
-    courseId: "CSC1001",
-    courseName: "Introduction to Computer Science",
-    courseDescription: "This course is an introduction to computer science.",
-    examinerName: "Dr. John Doe",
-  };
-  function requestCourseHandler() {
-    fetch(
-      "https://react-getting-started-59f1b-default-rtdb.firebaseio.com/courses.json",
+  let [courseDetails, setCourseDetails] = useState([]);
+  const location = useLocation();
+  const { courseId } = location.state;
+  const history = useNavigate();
+
+  useEffect(() => {
+    getCourseDetails();
+  }, []);
+
+  let getCourseDetails = async () => {
+    let response = await fetch(
+      "http://localhost:8000/main_app/coursedetail/" + courseId,
       {
-        method: "POST",
-        body: JSON.stringify({
-          requestType: requestType,
-          courseId: DUMMY_DATA.courseId,
-          studentEmail: email,
-        }),
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
+          Authorization: "Bearer " + String(userCtx.authTokens.access),
         },
       }
-    ).then(() => {
-      console.log("Course requested");
-    });
-  }
+    );
+    let data = await response.json();
+
+    if (response.status === 200) {
+      setCourseDetails(data);
+    } else if (response.statusText === "Unauthorized") {
+      userCtx.logoutUser();
+    }
+  };
+
+  let requestCourseHandler = async (e) => {
+    e.preventDefault();
+    let response = await fetch(
+      "http://localhost:8000/main_app/coursejoin/" + courseId,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + String(userCtx.authTokens.access),
+        }
+      }
+    );
+    let data = await response.json();
+    if (response.status === 200) {
+      courseRequested = true;
+      userCtx.setAuthTokens(data);
+      userCtx.setUser(jwt_decode(data.access));
+      localStorage.setItem("authTokens", JSON.stringify(data));
+      history("/");
+    } else {
+      alert("Something went wrong!");
+    }
+  };
+
   return (
     <section>
-      <CourseInfo
-        courseData={DUMMY_DATA}
-      />
+      <CourseInfo courseData={courseDetails} />
       <div>
         <button type="submit" onClick={requestCourseHandler}>
           {courseRequested ? "Cancel Request" : "Enroll"}
         </button>
       </div>
       <div>
-        <Link to="/"><button type="button">Back</button></Link>
+        <Link to="/">
+          <button type="button">Back</button>
+        </Link>
       </div>
     </section>
   );
