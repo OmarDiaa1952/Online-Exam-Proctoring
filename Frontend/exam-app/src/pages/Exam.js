6b89b80180f26b4446ca36e29eb6e6a49f781e46
Ahmed Alaa <1808283@eng.asu.edu.eg>
2023-03-27 17:15:41 +0200

    Updated Exam page
    Added function to parse date string and return the required format for date for start_date and submission_date

--- Frontend/exam-app/src/pages/Exam.js
+++ Frontend/exam-app/src/pages/Exam.js
@@ -9,13 +9,32 @@ function ExamPage() {
   const examId = userCtx.examId;
   const [examQuestions, setExamQuestions] = useState([]);
   const [userAnswers, setUserAnswers] = useState([]);
+  const [startTime, setStartTime] = useState("");
   const history = useNavigate();
+
+  function dateConverter(date) {
+    let year = date.split("/")[2].split(",")[0];
+    let month = date.split("/")[0];
+    month = month.length === 1 ? "0" + month : month;
+    let day = date.split("/")[1];
+    day = day.length === 1 ? "0" + day : day;
+    let morning = date.split(",")[1].split(" ")[2] === "AM";
+    let hour = date.split(" ")[1].split(":")[0];
+    hour = morning ? (hour.length === 1 ? "0" + hour : hour) : hour * 1 + 12;
+    let minute = date.split(" ")[1].split(":")[1];
+    minute = minute.length === 1 ? "0" + minute : minute;
+    let second = date.split(" ")[1].split(":")[2];
+    second = second.length === 1 ? "0" + second : second;
+    let convertedDate = year + "-" + month + "-" + day + "T" + hour + ":" + minute + ":" + second;
+    return convertedDate;
+  }
 
   useEffect(() => {
     getExamQuestions();
   }, []);
 
   let getExamQuestions = async () => {
+    let current_date = dateConverter(new Date().toLocaleString());
     let response = await fetch(
       "http://localhost:8000/main_app/questionlist/" + examId,
       {
@@ -39,6 +58,7 @@ function ExamPage() {
           choice4: question.choice_4,
         }))
       );
+      setStartTime(current_date);
     } else if (response.statusText === "Unauthorized") {
       userCtx.logoutUser();
     }
@@ -46,6 +66,13 @@ function ExamPage() {
 
   let finishHandler = async (e) => {
     e.preventDefault();
+    console.log(userAnswers);
+    const answers = userAnswers.map((answer) => ({
+      question_id: answer.questionId,
+      choice: answer.answer,
+    }));
+    console.log(answers);
+    const submissionTime = dateConverter(new Date().toLocaleString());
     let response = await fetch("http://localhost:8000/main_app/examend", {
       method: "POST",
       headers: {
@@ -54,15 +81,10 @@ function ExamPage() {
       },
       body: JSON.stringify(
         {
-          exam_id: 1,
-            start_time: "2023-02-28T15:40:52",
-            submission_time: "2023-02-28T15:40:54",
-            answers: [
-                {
-                    "question_id": 1,
-                    "choice": 2
-                }
-            ]
+          exam_id: userCtx.examId,
+          start_time: startTime,
+          submission_time: submissionTime,
+          answers: answers
         }),
     });
     if (response.status === 200) {
@@ -83,8 +105,8 @@ function ExamPage() {
       } else {
         let newUserAnswers = [...prevUserAnswers];
         newUserAnswers[userAnswerIndex] = {
-          question_id: questionId,
-          choice: answer,
+          questionId: questionId,
+          answer: answer,
         };
         return newUserAnswers;
       }
