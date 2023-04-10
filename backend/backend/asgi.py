f0a2b9fda0ab1ded3a18567305fdfe1a4e87c6be
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-04-10 13:21:21 +0200

    adding web sockets

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,10 +1,18 @@
 import os
 
-from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+import main_app.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            main_app.routing.websocket_urlpatterns
+        )
+    ),
 })
