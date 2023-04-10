06b4aa45939d64383cdf250f56d999f8c5e8f080
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-04-10 13:21:05 +0200

    commenting the whole file

--- backend/backend/routing.py
+++ backend/backend/routing.py
@@ -1,5 +1,11 @@
-# from channels.routing import ProtocolTypeRouter
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# import main_app.routing
 
 # application = ProtocolTypeRouter({
-#     # (http->django views is added by default)
+#     'websocket': AuthMiddlewareStack(
+#         URLRouter(
+#             main_app.routing.websocket_urlpatterns
+#         )
+#     ),
 # })
