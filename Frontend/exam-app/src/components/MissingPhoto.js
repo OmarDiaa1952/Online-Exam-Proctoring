242b7023e4083dea3146d3843a0737d7b30de964
Ahmed Alaa <1808283@eng.asu.edu.eg>
2023-04-26 18:02:18 +0200

    Created a new component to give a warning for missing photo
    Created MissingPhoto Js component file and an empty css file for it

---
+++ Frontend/exam-app/src/components/MissingPhoto.js
@@ -0,0 +1,13 @@
+import classes from "./MissingPhoto.module.css";
+
+function MissingPhoto() {
+  return (
+    <div>
+      <p>
+        Warning! You have not uploaded a photo yet. Please upload a photo to be able to join exams.
+      </p>
+    </div>
+  );
+}
+
+export default MissingPhoto;
