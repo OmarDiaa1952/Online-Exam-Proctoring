697542fd1f50da347685c87b95537c888a3f24b0
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-04-29 15:32:22 +0300

    import jwtauth... from middlewares file and using it

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -2,7 +2,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+# from channels.auth import AuthMiddlewareStack
+from main_app.middlewares import JwtAuthMiddlewareStack
 
 import main_app.routing
 
@@ -10,7 +11,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "websocket": JwtAuthMiddlewareStack(
         URLRouter(
             main_app.routing.websocket_urlpatterns
         )
