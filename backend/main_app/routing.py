3397a2c2d2511f01d194faed6da9ae51030e28bf
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-04-29 15:25:43 +0300

    removing stude id from url

--- backend/main_app/routing.py
+++ backend/main_app/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from .consumers import ExamConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/exam/(?P<exam_id>\d+)/(?P<student_id>\d+)/$', ExamConsumer.as_asgi()),
+    re_path(r'ws/exam/(?P<exam_id>\d+)/$', ExamConsumer.as_asgi()),
 ]
