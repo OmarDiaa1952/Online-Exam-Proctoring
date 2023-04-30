0850c2ae26793d3845c0511f3c4ccc31b4aca181
Ahmed Alaa <1808283@eng.asu.edu.eg>
2023-04-30 23:37:34 +0300

    Updated UseWindowDimensions.js
    Added warning messages if the screen is not in fullscreen or not maximzed.
    Added props to track the status of the window and updated the logic of the component

--- Frontend/exam-app/src/utils/UseWindowDimensions.js
+++ Frontend/exam-app/src/utils/UseWindowDimensions.js
@@ -1,59 +1,115 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
   return {
     width,
-    height
+    height,
   };
 }
 
-export default function UseWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+export default function UseWindowDimensions(props) {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  const [maxWindowDimensions, setMaxWindowDimensions] = useState({
+    maxWidth: window.innerWidth,
+    maxHeight: window.innerHeight,
+  });
+
+  const [windowStatus, setWindowStatus] = useState("fullScreen");
+  const [initialized, setInitialized] = useState(false);
+
+  let fullScreenWarningDiv = (
+    <div>
+      <p>
+        Warning, It is prefered to use full screen mode for the exam. If you
+        want to use full screen mode, please click on F11 button.
+      </p>
+    </div>
+  );
+
+  let maximizedWarningDiv = (
+    <div>
+      <p>
+        Warning, You must keep the window maximized otherwise the exam will be
+        ended!
+      </p>
+    </div>
+  );
+
+  useEffect(() => {}, [windowStatus]);
 
   useEffect(() => {
-    console.log(window.screen.availWidth);
-    console.log(window.screen.availHeight);
-    console.log(window.screen.width);
-    console.log(window.screen.height);
-    console.log(window.innerWidth);
-    console.log(window.innerHeight);
     function handleResize() {
       setWindowDimensions(getWindowDimensions());
-      CheckWindow();
+      if (
+        maxWindowDimensions.maxWidth < window.innerWidth ||
+        maxWindowDimensions.maxHeight < window.innerHeight
+      ) {
+        setMaxWindowDimensions({
+          maxWidth: window.innerWidth,
+          maxHeight: window.innerHeight,
+        });
+      }
+      if (!initialized) {
+        wait();
+        setInitialized(true);
+        return;
+      }
+      if (
+        window.innerWidth === maxWindowDimensions.maxWidth &&
+        window.innerHeight === maxWindowDimensions.maxHeight
+      ) {
+        setWindowStatus("fullScreen");
+        props.onChangeWindowDimensions(1);
+        console.log("window is in full Screen");
+      } else if (
+        window.screen.availWidth === window.outerWidth &&
+        window.screen.availHeight === window.outerHeight
+      ) {
+        setWindowStatus("maximized");
+        props.onChangeWindowDimensions(1);
+        console.log("Window is maximized");
+      } else {
+        setWindowStatus("notMaximized");
+        props.onChangeWindowDimensions(0);
+        console.log("window is not maximized");
+      }
     }
-    console.log(windowDimensions);
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [windowDimensions, initialized]);
 
-  function CheckWindow() {
+  let wait = async () => {
+    await timeout(100);
+  };
 
-    var A = window.screen.availWidth;
-    var AA = window.outerWidth;
-  
-    var B = window.screen.availHeight;
-    var BB = window.outerHeight;
-    
-    if (A === AA && B === BB) {
-    
-      console.log("Window is maximized or in full screen");
-  
-    }
-     
-    else {
-     
-      console.log("Window is not maximized or in full screen");
-      
-     }
-  
+  let timeout = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  function getDimensions() {
+    console.log("availWidth: " + window.screen.availWidth);
+    console.log("availHeight: " + window.screen.availHeight);
+    console.log("outerWidth: " + window.outerWidth);
+    console.log("outerHeight: " + window.outerHeight);
+    console.log("width: " + window.screen.width);
+    console.log("height: " + window.screen.height);
+    console.log("innerWidth: " + window.innerWidth);
+    console.log("innerHeight: " + window.innerHeight);
+    console.log("maxWidth: " + maxWindowDimensions.maxWidth);
+    console.log("maxHeight: " + maxWindowDimensions.maxHeight);
   }
 
   return (
     <div>
-      <h1>Window Dimensions</h1>
-      <p>Width: {windowDimensions.width}</p>
-      <p>Height: {windowDimensions.height}</p>
+      {windowStatus === "maximized"
+        ? fullScreenWarningDiv
+        : windowStatus === "notMaximized"
+        ? maximizedWarningDiv
+        : null}
     </div>
   );
 }
