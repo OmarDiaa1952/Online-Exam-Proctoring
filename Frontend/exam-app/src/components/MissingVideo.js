6e9a1020cde05b3900418c845f332651afe2871f
Ahmed Alaa <1808283@eng.asu.edu.eg>
2023-05-08 18:10:32 +0300

    Updated Missing message

---
+++ Frontend/exam-app/src/components/MissingVideo.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import classes from "./MissingVideo.module.css";
+
+function MissingPhoto() {
+  return (
+    <div>
+      <p>
+        Warning! You have to record a video of yourself to be able to join exams.
+      </p>
+      <Link to="/video">Record Video</Link>
+    </div>
+  );
+}
+
+export default MissingPhoto;
