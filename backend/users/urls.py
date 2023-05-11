78cb4b536b709bcb77319da1d302bfa9ec68fdc7
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-05-11 18:19:35 +0300

    editing url

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path("photoupload", PhotoUploadView.as_view(), name="PhotoUpload"),
     path("photoretrieve", PhotoRetrieve.as_view(), name="PhotoRetrieve"),
     path("photoexists", PhotoExistsView.as_view(), name="PhotoExists"),
-    path("videoupload", VideoUploadView.as_view(), name="VideoUpload"),
+    path("registrationvideoupload", RegistrationVideoUploadView.as_view(), name="VideoUpload"),
     path("videoexists", VideoExistsView.as_view(), name="VideoExists"),
 ]
