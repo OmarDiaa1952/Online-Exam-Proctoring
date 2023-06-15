6fe7d074bbdeaafdb6cfe688e1d8b470fa76ec05
Ahmed Alaa <1808283@eng.asu.edu.eg>
2023-06-15 20:35:59 +0300

    Updated Timer

--- Frontend/exam-app/src/utils/VideoTimer.js
+++ Frontend/exam-app/src/utils/VideoTimer.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 const VideoTimer = (props) => {
-  const [seconds, setSeconds] = useState(30);
+  const [seconds, setSeconds] = useState(20);
   const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
