fd8316e5f20d1e8e9e3c2f1c435c704df7f011c7
Moaaz Hamdy <moaaz.m.hamdy@gmail.com>
2023-06-21 21:47:22 +0300

    drop menu icon fix

--- Frontend/exam-app/src/components/NavBar.js
+++ Frontend/exam-app/src/components/NavBar.js
@@ -87,6 +87,11 @@ function NavBar() {
                     d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
                   />
                 </svg>
+                
+                <span className="mx-2"></span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                  <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                </svg>
               </button>
               <div className="card text-start">
                 <div className="card-body px-4 py-4">
