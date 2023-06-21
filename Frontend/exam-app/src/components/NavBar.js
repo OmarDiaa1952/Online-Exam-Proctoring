ca43f0af6047565b663b7a978151ec167bfd6abd
Ahmed Alaa <1808283@eng.asu.edu.eg>
2023-06-21 22:27:02 +0300

    Added arrow in the navbar

--- Frontend/exam-app/src/components/NavBar.js
+++ Frontend/exam-app/src/components/NavBar.js
@@ -22,6 +22,20 @@ function NavBar() {
       <span className="text-warning">OT</span>
     </span>
   );
+
+  const svgIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      fill="#6c757d"
+      className="bi bi-caret-right-fill"
+      viewBox="0 0 16 16"
+    >
+      <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+    </svg>
+  );
+
   const userCtx = useContext(UserContext);
   const [proctobotSpan, setProctobotSpan] = useState(proctobotSpan1);
   const navigate = useNavigate();
@@ -38,9 +52,13 @@ function NavBar() {
           </h1>
         </a>
       </div>
-      <div className="container-fluid col-1"></div>
-      {(userCtx.courseId && userCtx.courseId !== "null") ? (
-        <div className="container-fluid col-1">
+      {userCtx.courseId && userCtx.courseId !== "null" ? (
+        <div className="container-fluid col-1">{svgIcon}</div>
+      ) : (
+        <div className="container-fluid col-1"></div>
+      )}
+      {userCtx.courseId && userCtx.courseId !== "null" ? (
+        <div className="container-fluid col-2">
           <a className="navbar-brand" href="/course-details">
             <h2 className="text-light">
               <span className="green-hover">{userCtx.courseName}</span>
@@ -50,8 +68,12 @@ function NavBar() {
       ) : (
         <div className="container-fluid col-1"></div>
       )}
-      <div className="container-fluid col-1"></div>
-      {(userCtx.examId && userCtx.examId !== "null") ? (
+      {userCtx.examId && userCtx.examId !== "null" ? (
+        <div className="container-fluid col-1">{svgIcon}</div>
+      ) : (
+        <div className="container-fluid col-1"></div>
+      )}
+      {userCtx.examId && userCtx.examId !== "null" ? (
         <div className="container-fluid col-1">
           <a
             className="navbar-brand"
@@ -87,9 +109,16 @@ function NavBar() {
                     d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
                   />
                 </svg>
-                
+
                 <span className="mx-2"></span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-caret-down-fill"
+                  viewBox="0 0 16 16"
+                >
                   <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
                 </svg>
               </button>
